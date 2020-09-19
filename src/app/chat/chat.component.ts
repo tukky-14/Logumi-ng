684d1013d5639e776c294da98549aff756c8b5aa
Eiji Otsuki <bigoonage0104@gmail.com>
2020-09-19 22:49:18 +0900

    Fix chat form

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,13 +1,16 @@
 import { CommentStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { Comment } from '../class/chat';
+import { Comment, User } from '../class/chat'; 
 
-const COMMENTS: Comment[] = [ 
-  { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
-  { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
-  { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
+const CURRENT_USER: User = new User(1, 'Tanaka Jiro'); // 自分のUser情報を追加
+const ANOTHER_USER: User = new User(2, 'Suzuki Taro'); // 相手のUser情報を追加
+const COMMENTS: Comment[] = [ // クラスを元にコメントを作成
+  new Comment(ANOTHER_USER, 'Suzukiの１つ目のコメントです。'),
+  new Comment(ANOTHER_USER, 'Suzukiの2つ目のコメントです。'),
+  new Comment(CURRENT_USER, 'Tanakaの１つ目のコメントです。'),
+  new Comment(ANOTHER_USER, 'Suzukiの3つ目のコメントです。'),
+  new Comment(CURRENT_USER, 'Tanakaの2つ目のコメントです。')
 ];
-
 
 @Component({
   selector: 'app-chat',
@@ -18,6 +21,13 @@ const COMMENTS: Comment[] = [
 export class ChatComponent implements OnInit {
   public content:string = '';
   public comments = COMMENTS;
+  public current_user = CURRENT_USER;
+
+  addComment(comment: string) {
+    if (comment) {
+      this.comments.push(new Comment(this.current_user, comment));
+    }
+  }
 
   constructor() { }
 
--- src/app/class/chat.ts
+++ src/app/class/chat.ts
@@ -1,4 +1,19 @@
-export class Comment {
+export class User { // Userの定義を追加
+  uid: number;
   name: string;
+
+  constructor(uid: number, name: string) {
+    this.uid = uid;
+    this.name = name;
+  }
+}
+
+export class Comment { // Commentの定義を変更
+  user: User;
   content: string;
+
+  constructor(user: User, content: string) {
+    this.user = user;
+    this.content = content;
+  }
 }
