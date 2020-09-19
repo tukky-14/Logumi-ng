a0359d1ff8bdda1dccf4cb957ca1212a87ab5c8c
Eiji Otsuki <bigoonage0104@gmail.com>
2020-09-20 07:47:11 +0900

    Setting firestore

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,8 @@
 import { CommentStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Comment, User } from '../class/chat'; 
+import { AngularFirestore } from '@angular/fire/firestore'; 
+import { Observable } from 'rxjs'; 
 
 const CURRENT_USER: User = new User(1, 'Tanaka Jiro'); // 自分のUser情報を追加
 const ANOTHER_USER: User = new User(2, 'Suzuki Taro'); // 相手のUser情報を追加
@@ -19,6 +21,7 @@ const COMMENTS: Comment[] = [ // クラスを元にコメントを作成
 })
 
 export class ChatComponent implements OnInit {
+  item: Observable<Comment>; 
   public content:string = '';
   public comments = COMMENTS;
   public current_user = CURRENT_USER;
@@ -27,9 +30,14 @@ export class ChatComponent implements OnInit {
     if (comment) {
       this.comments.push(new Comment(this.current_user, comment));
     }
-  }
+ }
 
-  constructor() { }
+  constructor(db: AngularFirestore) {
+    this.item = db
+      .collection('comments')
+      .doc<Comment>('item')
+      .valueChanges();
+  }
 
   ngOnInit(): void {
   }
