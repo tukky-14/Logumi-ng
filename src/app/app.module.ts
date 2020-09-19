83b7ab8bdc0ef8bef038e7bfac67812b64eb5f5d
Eiji Otsuki <bigoonage0104@gmail.com>
2020-09-19 22:36:32 +0900

    Update chat form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 import { environment } from '../environments/environment'; // Firebaseで追加
 import { AngularFireModule } from '@angular/fire'; // Firebaseで追加
 import { AngularFirestoreModule } from '@angular/fire/firestore'; // Firebaseで追加
@@ -11,12 +12,13 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ListComponent } from './list/list.component';
+import { ChatComponent } from './chat/chat.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DetailComponent } from './detail/detail.component';
-import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   // そのモジュールの中で宣言されているディレクティブ（コンポーネント）とパイプを登録する
@@ -35,6 +37,7 @@ import { ChatComponent } from './chat/chat.component';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase), // Firebaseで追加
     AngularFirestoreModule,  // Firebaseで追加
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,11 +1,23 @@
+import { CommentStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Comment } from '../class/chat';
+
+const COMMENTS: Comment[] = [ 
+  { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
+  { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
+  { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
+];
+
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
+
 export class ChatComponent implements OnInit {
+  public content:string = '';
+  public comments = COMMENTS;
 
   constructor() { }
 
